13c5ee9295f3ad38d1d8c996d2b5c7576085cc16
MrAkergren <oskar.akergren@gmail.com>
2015-06-25 12:48:12 +0200

    first draft

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,24 +1,53 @@
 #! /usr/bin/env python3
 # -*- coding: UTF-8 -*-
-import sys
 
-while True:
-    user_choice = input("Vad vill du dela? heltal eller flyttal? (h/f)  ")\
-                    .lower()
-    
-    if user_choice == "quit":
-        sys.exit(1)
+"""
+Oskar Åkergren
+Mac OS X 10.9.5
+Python 3.4.1
+"""
 
-    elif user_choice != "h" and user_choice != "f":
-        print("Ange ett giltigt värde")
+def main():
+    while True:
+        user_choice = input("Hej, ska det vara hel- eller flyttalsdivision? (h/f):"
+                            ).lower()
 
-    elif user_choice == "h":
-        dividend = int(input("Vilket tal ska delas?\n"))
-        divisor  = int(input("Med vad ska det delas?\n"))
-        print(dividend / divisor)
+        if user_choice != "h" and user_choice != "f":
+            print("Tyvärr, dina möjliga val är h/H eller f/F")
 
-    else:
-        dividend = float(input("Vilket tal ska delas?\n"))
-        divisor  = float(input("Med vad ska det delas?\n"))
-        print(dividend / divisor)
-    print("")
+        elif user_choice == "h":
+            intnum()
+            break
+
+        else:
+            floatnum()
+            break
+
+
+def intnum():
+    try:
+        dividend = int(input("Skriv in din dividend:  "))
+        divisor = int(input("Skriv in din divisor:  "))
+        prettyprint(dividend, divisor)
+    except:
+        print("Ange ett heltal")
+        intnum()
+
+
+def floatnum():
+    try:
+        dividend = float(input("Skriv in din dividend:  "))
+        divisor = float(input("Skriv in din divisor:  "))
+        prettyprint(dividend, divisor)
+    except:
+        print("Ange ett flyttal")
+        floatnum()
+
+
+def prettyprint(dividend, divisor):
+    print(
+        "\tResultatet av %g / %g = %g" %
+        (dividend, divisor, (dividend / divisor))
+    )
+
+main()
